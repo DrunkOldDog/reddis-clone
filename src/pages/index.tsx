5c9553a890d1064379c3e8208d9ef52c55f4f9ab
Juani Reyes <juanisoul@gmail.com>
2022-01-12 22:43:26 -0400

    Enhance Home Screen with redirect to signup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-import { Typography } from "@mui/material";
+import { useAuth } from "../context/auth";
+import { Button, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function Home() {
-  return <Typography variant="h4">Hello World</Typography>;
+  const { user } = useAuth();
+  return user ? (
+    <Typography variant="h4">Hello {user.getUsername()}!</Typography>
+  ) : (
+    <Link href={"/signup"}>
+      <Button>Sign Up here! </Button>
+    </Link>
+  );
 }
