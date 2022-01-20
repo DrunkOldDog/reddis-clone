a081304191139c807cfdf6398c3b02dadd89280d
Juani Reyes <juanisoul@gmail.com>
2022-01-19 23:51:13 -0400

    Integrate content and images to PostPreview

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -1,9 +1,12 @@
+import { useRouter } from "next/router";
 import styled from "@emotion/styled";
-import { Grid, IconButton, Paper, Typography } from "@mui/material";
+import Image from "next/image";
+import { Box, ButtonBase, Grid, IconButton, Paper, Typography } from "@mui/material";
 import {
   ArrowUpward as ArrowUpwardIcon,
   ArrowDownward as ArrowDownwardIcon,
 } from "@mui/icons-material";
+import { timeSince } from "../../common/helpers";
 import type { Post } from "../../API";
 
 interface PostPreviewProps {
@@ -20,18 +23,31 @@ VoteButton.defaultProps = {
   color: "inherit",
 };
 
+const VotesBox = styled(Box)`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const PostPreview: React.FC<PostPreviewProps> = ({ post }: PostPreviewProps) => {
+  const router = useRouter();
+
   return (
     <Paper elevation={3}>
       <StyledPostPreview direction="row" spacing={3} container>
-        <Grid item spacing={1} direction="column">
-          <Grid container direction={"column"} alignItems={"center"}>
+        <Grid item sm={1} xs={2}>
+          <Grid container direction={"column"} alignItems={"center"} spacing={1}>
             <Grid item>
               <VoteButton>
                 <ArrowUpwardIcon />
               </VoteButton>
             </Grid>
-            <Grid item>votes</Grid>
+            <Grid item>
+              <VotesBox>
+                <Typography variant="h6">{post.upvotes - post.downvotes}</Typography>
+                <Typography variant="body2">votes</Typography>
+              </VotesBox>
+            </Grid>
             <Grid item>
               <VoteButton>
                 <ArrowDownwardIcon />
@@ -40,13 +56,31 @@ const PostPreview: React.FC<PostPreviewProps> = ({ post }: PostPreviewProps) => 
           </Grid>
         </Grid>
 
-        <Grid item>
-          <Grid container direction="column">
-            <Typography variant="body1">
-              Posted by <b>{post.owner}</b> at <b>{post.createdAt}</b>
-            </Typography>
-            <Typography variant={"h2"}>{post.title}</Typography>
-          </Grid>
+        <Grid item sm={11} xs={10}>
+          <ButtonBase onClick={() => router.push(`/post/${post.id}`)} style={{ width: "100%" }}>
+            <Grid container direction="column" alignItems={"flex-start"}>
+              <Typography variant="body1">
+                Posted by <b>{post.owner}</b> {timeSince(new Date(post.createdAt))} ago
+              </Typography>
+              <Typography variant={"h2"}>{post.title}</Typography>
+              <Typography
+                variant="body1"
+                style={{
+                  maxHeight: 64,
+                  overflowY: "hidden",
+                  textAlign: "start",
+                }}
+              >
+                {post.contents}
+              </Typography>
+
+              <Grid item>
+                {post.image && (
+                  <Image src={post.image} height={270} width={420} layout="intrinsic" />
+                )}
+              </Grid>
+            </Grid>
+          </ButtonBase>
         </Grid>
       </StyledPostPreview>
     </Paper>
